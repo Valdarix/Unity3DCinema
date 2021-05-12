93ede4851400945926245c5a4b090c3154eca9e6
Valdarix <jwest@satori-assoc.com>
2021-05-12 09:55:53 -0400

    Added throw coin animation

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea3490d1b2ba183ce30bee9ff7dc2de513bb6047a4c6142bbc129da777a2f2f6
-size 1263
+oid sha256:f4d85d193b097f203ae9df69a67a68d5f24e34c0cf711e391b20cd690b0a88d2
+size 1934
