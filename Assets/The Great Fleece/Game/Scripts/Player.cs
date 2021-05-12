d381b135c8bf5d9fd401eeba084333f3dc6f0505
Valdarix <jwest@satori-assoc.com>
2021-05-12 09:24:02 -0400

    Added player animation for walk and idle, setup throw

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53347c3db65d350106a499f67bb7e3f3a93ee46828d4f6e278a3c26309e973d1
-size 887
+oid sha256:ea3490d1b2ba183ce30bee9ff7dc2de513bb6047a4c6142bbc129da777a2f2f6
+size 1263
