f9f214cdc8021760f03b47f67de622d545cf3921
Valdarix <jwest@satori-assoc.com>
2021-05-13 20:27:06 -0400

    Completed Guard AI

--- Assets/The Great Fleece/Game/Scripts/CameraLookat.cs
+++ Assets/The Great Fleece/Game/Scripts/CameraLookat.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:876f0afca2ca17f46d2fc44db139573691d0ecb65eb8e1944ec33f3a51ce3bb1
-size 339
+oid sha256:c44dbe829aeb0f22b2227736cdf853ca1c1202a79ada6139d452dd77f932e0d7
+size 587
